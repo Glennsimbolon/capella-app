034e4ec93e52b51848eb6c9fdc4ce1be7f37de05
Glennsimbolon <glennrezasimbolon@gmail.com>
2026-08-15 14:09:05 +0700

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/capella-credit-app-v2/',
+  base: '/capella-app/',  // <-- PAKAI NAMA REPO!
   build: {
     sourcemap: false,
     outDir: 'dist'
